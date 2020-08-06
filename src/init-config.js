c19147c31701bd9425311fde4c220364a81221d9
Ramon Rangel Osorio <ramon.rangel@protonmail.com>
2020-08-06 09:18:57 -0500

    Adding code documentation.

--- src/init-config.js
+++ src/init-config.js
@@ -4,6 +4,15 @@ const ini = require('ini');
 const assignIn = require('lodash.assignin');
 const appRoot = require('app-root-path');
 
+/**
+ * Main function.
+ * @param folder Absolute path to your file. (Default root package path).
+ * @param name Name of file. (Default 'config').
+ * @param ext File extension. (Default 'ini').
+ * @param context This will be the access name of configurations. (Default 'iniconf').
+ * @external `EXT_INI_CONF`: External file path. Optional.
+ * @throws `File not found`
+ */
 const conf = ({folder, name = 'config', ext = 'ini', context = 'iniconf'} = {}) => {
 
     let path = appRoot.path;
@@ -30,6 +39,11 @@ const conf = ({folder, name = 'config', ext = 'ini', context = 'iniconf'} = {}) 
     
 }
 
+/**
+ * Load and update the context.
+ * @param {*} path 
+ * @param {*} context 
+ */
 function load(path, context) {
     if (fs.existsSync(path)) {
         global[context] = assignIn(global[context], ini.parse(fs.readFileSync(path, 'utf-8')));
