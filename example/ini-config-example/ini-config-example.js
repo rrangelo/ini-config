4bbb03a9eb275b347b365aabfa7f772db6318d00
Ramon Rangel Osorio <ramon.rangel@protonmail.com>
2020-08-06 13:52:07 -0500

    Adding example project.

---
+++ example/ini-config-example/ini-config-example.js
@@ -0,0 +1,22 @@
+'use strict'
+let ini = require('@rrangelo/ini-config');
+
+ini();
+
+console.log('main config:         ' + JSON.stringify(iniconf));
+
+ini({name: 'other', ext: 'conf'});
+
+console.log('named config:        ' + JSON.stringify(iniconf));
+
+ini({folder: 'config'});
+
+console.log('folded config:       ' + JSON.stringify(iniconf));
+
+ini({folder: 'config', name: 'another', context: 'inictx'});
+
+console.log('folded named config: ' + JSON.stringify(inictx));
+
+if (process.env.EXT_INI_CONF) {
+    process.env.EXT_INI_CONF = undefined;
+}
